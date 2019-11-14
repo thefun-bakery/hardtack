1d5eae1db3900b34d3098519cbcafce2f90ca0e4
thefun.sammy <thefun.sammy@gmail.com>
2019-11-14 17:27:15 +0000

    #1 production 환경의 db연결 정보 암호화

--- bin/docker_build.sh
+++ bin/docker_build.sh
@@ -1,3 +1,3 @@
 cd $(dirname -- "$0")
 
-docker build --rm --no-cache -t hardtack -f ../dockerfiles/real ..
+docker build --build-arg DB_HOST --build-arg DB_PORT --build-arg DB_USER --build-arg DB_PASSWORD --build-arg DB_NAME --rm --no-cache -t hardtack -f ../dockerfiles/real ..
---
+++ bin/docker_hardtack.sh
@@ -0,0 +1,1 @@
+ docker run --rm -d -p80:3000 -e RAILS_ENV=production -e RAILS_MASTER_KEY=6e9ec50dd719b05f88daebb209d85a35 hardtack
