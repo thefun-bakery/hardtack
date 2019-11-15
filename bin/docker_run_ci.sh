79fefffa5a45f86f296a3eaf0c2e859d423f8915
thefun.sammy <thefun.sammy@gmail.com>
2019-11-15 17:59:15 +0000

    #1 개발환경에서 docker 실행 시 master key 누락한거 추가

--- bin/docker_run_ci.sh
+++ bin/docker_run_ci.sh
@@ -1,1 +1,1 @@
-docker run --name hardtack-ci --rm -d -p3000:3000 hardtack-ci
+docker run --name hardtack-ci --rm -d -p3000:3000 -e RAILS_MASTER_KEY=$RAILS_MASTER_KEY hardtack-ci
--- libexec/entry_point.sh
+++ libexec/entry_point.sh
@@ -1,4 +1,5 @@
 /usr/bin/supervisord
 
 cd /hardtack
+rails db:migrate
 rails server -b 0.0.0.0
